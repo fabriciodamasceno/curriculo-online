e19223d7bcb47200eb902e357ee1277667da0808
Fabrício M. Damasceno <fabricio.damasceno@outlook.com.br>
2022-11-14 11:15:50 -0300

    Estilizando botões redes sociais

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,15 @@
-import FotoPerfil from '../img/foto.jpg'
+import SocialNetworks from './SocialNetworks';
 
-import "../styles/components/sidebar.sass"
+import FotoPerfil from '../img/foto.jpg';
+
+import '../styles/components/sidebar.sass';
 
 const Sidebar = () => {
     return (
         <aside id="sidebar">
             <img src={FotoPerfil} alt="Fabrício Damasceno" />
             <p className="title">Desenvolvedor Full Stack</p>
-            <p>Redes Sociais</p>
+            <SocialNetworks />
             <p>Contato</p>
             <a href="#" className="btn">
                 Download Currículo
