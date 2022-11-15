8c09b79bdf25d3d49d9c4c23f08e2b50c41eba28
Fabrício M. Damasceno <fabricio.damasceno@outlook.com.br>
2022-11-15 16:11:00 -0300

    Finalizando a parte de Tecnologia do projeto.

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const ProjectsContainer = () => {
   return (
-    <div>
-      Projetos
-    </div>
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe maxime beatae natus, delectus accusamus a. Velit quas quaerat itaque, expedita fugiat dolore dolor, accusamus voluptatem explicabo molestias ad consequatur repudiandae.</p>
+      <a href="#" className="btn">
+        Ver Projetos
+      </a>
+    </section>
   )
 }
 
